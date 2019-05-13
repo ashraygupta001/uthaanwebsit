1e6348071d29bab7f6db8587bb8007d907f89e7a
ashray79gupta@gmail.com <ashray79gupta@gmail.com>
2019-05-13 16:11:39 +0530

    r changes. Lines starting

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Uthaan</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script rel="stylesheet" src="http://code.jquery.com/jquery-2.1.4.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
+<h1> hi</h1>
+
+
+
+
+
+</body>
+</html>
